5b2afa798b2de3d5853fc782c8d1a5b6df40bab3
Cedric <Cedric.sheow02@gmail.com>
2026-04-30 17:13:25 +0800

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,29 @@
+# there are a few data types in python that we will cover in this tutorial, including:
+# - int: for integers (whole numbers)
+# - float: for floating-point numbers (decimal numbers)
+# - str: for strings (text)
+# - bool: for boolean values (True or False)
+
+
+# example for each data type: 
+
+# integer (int)
+age = 24
+type(age)
+print (type(age)) # this will return the type of variable age, which is an integer (int)
+
+# float (float)
+height = 2.5
+type(height)
+print (type(height)) # this will return the type of variable height, which is a float
+
+# string (str)
+name = 'Python programming'
+type(name)
+print (type(name)) # this will return the type of variable name, which is a string
+
+
+# boolean (bool)
+is_programming_fun = True #often true or false statements, which 1s a boolean data type
+type(is_programming_fun)
+print (type(is_programming_fun)) # this will return the type of variable is_programming
