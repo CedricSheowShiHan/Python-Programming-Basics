67b99e3c68d5a7d13dc119c7c715c0998c9cffa3
Cedric <Cedric.sheow02@gmail.com>
2026-04-30 17:40:06 +0800

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,19 @@
+# In this example we will be learning about variables, which are used to store data in a program. Variables can hold different types of data, such as numbers, strings, and more. We will also learn about basic syntax rules, case sensitivity, and how to perform simple operations with variables.
+
+# declare and assign of variables
+
+#example: 
+
+age = 25 # this is an example of declaring a variable named 'age' and assigning it the value of 25.
+name = 'John'
+height = 1.73 # this is an example of declaring a variable named 'height' and assigning it the value of 1.73, and assumption here is in metres
+is_student = True # this is an example of declaring a variable named 'is_student' and assigning it the value of True, which is a boolean value.
+is_not_student = False # this is an example of declaring a variable named 'is_not_student' and assigning it the value of False, which is a boolean value.
+
+# print out the variables
+print(age) # this will print the value of the variable 'age', which is 25.
+print(name) # this will print the value of the variable 'name', which is 'John
+print(height) # this will print the value of the variable 'height', which is 1.73.
+print(is_student) # this will print the value of the variable 'is_student', which is True.
+print(is_not_student) # this will print the value of the variable 'is_not_student', which is False. 
+
