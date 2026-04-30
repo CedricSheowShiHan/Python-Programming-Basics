f2c34e88f055a9c316aa806e2413bf6e2d51b893
Cedric <Cedric.sheow02@gmail.com>
2026-04-30 16:30:03 +0800

    Add files via upload

---
+++ basic_code.py
@@ -0,0 +1,16 @@
+# welcome to my python programming code tutorial! 
+# In this tutorial, we will cover the basics of Python programming, including variables, data types, control structures, functions, and more. Let's get started!
+
+# Basics of Syntax rules
+
+## case sensitivity
+
+# declaring variable name:
+name = 'cedric' # we are free to use either ' or " when declaring variable
+Name  = 'Cedric' # this is a different variable from 'name' because of case sensitivity
+
+# lets try to print out the variables: 
+
+print (name)
+print (Name)
+
