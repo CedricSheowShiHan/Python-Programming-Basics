0a523fffd809a9b759268de1df7282eac20564eb
Cedric <Cedric.sheow02@gmail.com>
2026-04-30 17:25:09 +0800

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,28 @@
+# operators are basically ur + - * / % ** // etc
+
+# giving some examples: 
+
+a = 10 
+b = 5 
+
+addition = a + b
+print (addition) # this will print out the result of the addition, which is 15
+
+subtraction = a - b
+print (subtraction) # this will print out the result of the subtraction, which is
+
+multiplication = a * b
+print (multiplication) # this will print out the result of the multiplication, which is
+
+division = a / b
+print (division) # this will print out the result of the division, which is 2
+
+floor_division = a // b
+print (floor_division) # this will print out the result of the floor division, which is 2
+
+mod = a % b
+print (mod) # this will print out the result of the modulo operation, which is 0
+
+exponentiation = a ** b
+print (exponentiation) # this will print out the result of the exponentiation, which
+
